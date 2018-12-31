8438bb8f1bc2c7b0d39d30586b99c7913b243918
jlang2 <jlang2@ncsu.edu>
2018-12-31 16:34:34 -0500

    wrote addTopNav.js to automatically add topnav to each page

---
+++ addTopNav.js
@@ -0,0 +1,41 @@
+/**
+ * Appends topnav bar to a page.
+ */
+
+window.onload = function() {
+  // Create topnav containers
+  var db = document.body;
+  var topnav = document.createElement("nav");
+  var logoDiv = document.createElement("div");
+  var tabDiv = document.createElement("div");
+  topnav.id = "topnav";
+  tabDiv.id = "top-nav-tab-group";
+  db.appendChild(topnav);
+  topnav.appendChild(logoDiv);
+  topnav.appendChild(tabDiv);
+
+  // Create content and append to logoDiv
+  var logoAnchor = document.createElement("a");
+  logoAnchor.id = "logo-box";
+  logoAnchor.href = "/";
+  var logoImg = document.createElement("img");
+  logoImg.src = "/logo-favicon.png";
+  logoImg.id = "logo";
+  var logoTxt = document.createElement("span");
+  logoTxt.id = "logo-name";
+  logoTxt.innerText = "Jasmine Lang";
+  logoAnchor.appendChild(logoImg);
+  logoAnchor.appendChild(logoTxt);
+  logoDiv.appendChild(logoAnchor);
+
+  // Create content and append to tabDiv
+  var links = ["/", "/about.html", "/gallery.html", "/contact.html"];
+  var labels = ["Home", "About", "Gallery", "Contact"];
+  for (var i = 0; i < links.length; i++) {
+    var tab = document.createElement("a");
+    tab.className = "top-nav-tab";
+    tab.href = links[i];
+    tab.innerText = labels[i];
+    tabDiv.appendChild(tab);
+  }
+}
