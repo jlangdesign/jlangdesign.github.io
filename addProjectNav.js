07a0258cc3988563c9ceae01f30b4a996bb0a656
jlang2 <jlang2@ncsu.edu>
2019-01-08 21:48:54 -0500

    started trying to format project page text line length

--- addProjectNav.js
+++ addProjectNav.js
@@ -1,8 +1,17 @@
 /**
  * Appends project nav to a page underneath the topnav.
+ * Sections: Overview | Process (Mission - Playthrough - Results) | Links
  */
 window.onload = function() {
   // Create containers for sections and subsections
+  let projNav = document.createElement("nav");
+  projNav.class = "tab-bar";
 
   // Create links and append
+  const links = ["#overview", "#process", "#links"];
+  const labels = ["Overview", "Process", "Links"];
+  const subLinks = ["#mission", "#playthrough", "#results"];
+  const subLabels = ["Mission", "Playthrough", "Results"];
 }
+
+// TODO add "to top" and "to bottom" buttons?
