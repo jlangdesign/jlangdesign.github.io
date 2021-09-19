b4f9957a56779f88b5a223950ddeabcb0cd05551
jlang2 <41656802+jlangdesign@users.noreply.github.com>
2021-09-19 17:15:39 -0400

    Automated footer loading too + fixed some spacing issues

--- addMainNav.js
+++ addMainNav.js
@@ -2,6 +2,7 @@
  * Appends topnav bar to a page.
  */
 window.onload = function() {
+  // Add topnav
   fetch('/topnav.html')
     .then(res => res.text())
     .then(text => {
@@ -9,5 +10,15 @@ window.onload = function() {
       topnav.id = 'topnav';
       topnav.innerHTML = text;
       document.body.appendChild(topnav);
+    });
+
+  // Add footer
+  fetch('/footer.html')
+    .then(res => res.text())
+    .then(text => {
+      const foot = document.createElement('footer');
+      foot.innerHTML = text;
+      let page = document.querySelector('#page');
+      page.appendChild(foot);
     });
 }
