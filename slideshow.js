6b43b24f9e4bbc3777118b35e3263e0e859ad0ef
jlang2 <41656802+jlangdesign@users.noreply.github.com>
2025-11-29 18:36:19 -0500

    Rewrote site using 11ty

--- slideshow.js
+++ slideshow.js
@@ -7,20 +7,20 @@
 let current = 0;
 let slides = document.getElementsByClassName("slide-img");
 
-setInterval(function() {
-	// Set opacity of all slides to 0
-	for (let i = 0; i < slides.length; i++) {
-		slides[i].style.opacity = 0;
-	}
+setInterval(function () {
+  // Set opacity of all slides to 0
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.opacity = 0;
+  }
 
-	// If current slide is last slide, loop back to first slide
-	// Else, go to next slide in array
-	if (current != slides.length - 1) {
-		current++;
-	} else {
-		current = 0;
-	}
+  // If current slide is last slide, loop back to first slide
+  // Else, go to next slide in array
+  if (current != slides.length - 1) {
+    current++;
+  } else {
+    current = 0;
+  }
 
-	// Set opacity of current slide to 1
-	slides[current].style.opacity = 1;
+  // Set opacity of current slide to 1
+  slides[current].style.opacity = 1;
 }, 5000); // 5000 ms = 5 s
